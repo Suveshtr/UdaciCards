7aaa5c725ed1b842cf26305b59a48829adf3516e
Suveshtr <suvesh.trivedi@gmail.com>
2017-12-02 15:06:41 +0530

    Add receive decks reducer

--- reducers/index.js
+++ reducers/index.js
@@ -1,4 +1,29 @@
-import { ADD_DECK_TITLE, ADD_CARD } from '../actions'
+import { ADD_DECK_TITLE, ADD_CARD, RECEIVE_DECKS } from '../actions'
+// view of the store/state for decks
+// decks : {
+//   React: {
+//     title: 'React',
+//     questions: [
+//       {
+//         question: 'What is React?',
+//         answer: 'A library for managing user interfaces'
+//       },
+//       {
+//         question: 'Where do you make Ajax requests in React?',
+//         answer: 'The componentDidMount lifecycle event'
+//       }
+//     ]
+//   },
+//   JavaScript: {
+//     title: 'JavaScript',
+//     questions: [
+//       {
+//         question: 'What is a closure?',
+//         answer: 'The combination of a function and the lexical environment within which that function was declared.'
+//       }
+//     ]
+//   }
+// }
 
 function decks ( state = {}, action ) {
   switch (action.type) {
@@ -17,6 +42,11 @@ function decks ( state = {}, action ) {
         [action.title] : addCard({...state[action.title]}, action)
       }
 
+    case RECEIVE_DECKS:
+      return {
+        ...state,
+        ...action.decks
+      }
     default: 
       return state
   }
