ff37c401b154d7a6b2f3abba361dc95c5162b0ae
Suveshtr <suvesh.trivedi@gmail.com>
2017-12-03 13:52:40 +0530

    Add notification api

--- utils/api.js
+++ utils/api.js
@@ -1,10 +1,12 @@
 import { AsyncStorage } from 'react-native'
+import { Permissions, Notifications } from 'expo'
 
 const DECK_STORAGE_KEY = 'UdaciCatds:decks'
+const DECK_NOTIFICATION_KEY = 'UdaciCards:notifications'
 
 export function saveDeckTitle(title) {
-  
-  return AsyncStorage.mergeItem( DECK_STORAGE_KEY, JSON.stringify({
+
+  return AsyncStorage.mergeItem(DECK_STORAGE_KEY, JSON.stringify({
     [title]: {
       title: title,
       questions: []
@@ -13,15 +15,68 @@ export function saveDeckTitle(title) {
 }
 
 export function addCardToDeck(title, card) {
-   return AsyncStorage.mergeItem( DECK_STORAGE_KEY, JSON.stringify({
-      [title]: {
-        title: title,
-        questions: [card]
-      }
-    }))
+  return AsyncStorage.mergeItem(DECK_STORAGE_KEY, JSON.stringify({
+    [title]: {
+      title: title,
+      questions: [card]
+    }
+  }))
 }
 
-export function getDecks () {
+export function getDecks() {
   return AsyncStorage.getItem(DECK_STORAGE_KEY)
-          .then(result => JSON.parse(result))
+    .then(result => JSON.parse(result))
+}
+
+export function clearLocalNotifications() {
+  return AsyncStorage.removeItem(DECK_NOTIFICATION_KEY)
+    .then(Notifications.cancelAllScheduledNotificationsAsync())
+}
+
+function createNotification() {
+  return {
+    title: 'Don\'t neglect your studies!',
+    body: 'You haven\'t reviewed any of your card decks today. Don\'t want to fall off course!',
+    ios: {
+      sound: true
+    },
+    android: {
+      sound: true,
+      priority: 'high',
+      sticky: false,
+      vibrate: true
+    }
+  }
+}
+
+export function setLocalNotification() {
+  AsyncStorage.getItem(DECK_NOTIFICATION_KEY)
+    .then(JSON.parse)
+    .then(data => {
+      if (data === null) {
+        Permissions.askAsync(Permissions.NOTIFICATIONS)
+          .then(({ status }) => {
+            if (status === 'granted') {
+              Notifications.cancelAllScheduledNotificationsAsync()
+
+              let tomorrow = new Date()
+              tomorrow.setDate(tomorrow.getDate() + 1)
+              tomorrow.setHours(20)
+              tomorrow.setMinutes(0)
+
+              Notifications.scheduleLocalNotificationAsync(
+                createNotification(),
+                {
+                  time: tomorrow,
+                  repeat: 'day',
+                }
+              )
+
+              AsyncStorage.setItem(DECK_NOTIFICATION_KEY, JSON.stringify(true))
+            }
+          }
+          )
+      }
+    }
+    )
 }
